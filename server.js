2bee0d7a78a6f9be1740154316047bd678e795e9
najwanan <theunnamedrd@gmail.com>
2022-05-30 20:47:32 -0700

    removing node modules

---
+++ server.js
@@ -0,0 +1,19 @@
+//console.log("May Node be with you")
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3000, function () {
+    console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname +"/index.html"); 
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})

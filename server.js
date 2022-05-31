3bd4cc5acd63f1e99435330f019facc2cce784db
najwanan <theunnamedrd@gmail.com>
2022-05-31 15:44:01 -0700

    added connection to mongodb database

--- server.js
+++ server.js
@@ -1,19 +1,56 @@
 //console.log("May Node be with you")
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const connectionString =
+  "mongodb+srv://najwanan:yQ1Mdl9z3Y7Tq1G8@cluster0.ccqge.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function () {
-    console.log('listening on 3000')
-})
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname +"/index.html"); 
-})
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+
+
+
+
+
+/*MongoClient.connect(connectionString, (err, client) => {
+  if (err) return console.error(err);
+    console.log("connected to database");
+    const db = client.db('star-wars-quotes')
+});*/
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log("Connected to Database");
+        const db = client.db("star-wars-quotes");
+        const quotesCollection = db.collection('quotes')
+
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({ extended: true }));
+
+        app.get("/", (req, res) => {
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', { quotes: results })
+                  console.log(results);
+                })
+                .catch(error => console.error(error))
+           
+          res.sendFile(__dirname + "/index.html");
+        })
+
+        app.post("/quotes", (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                //console.log(result)
+            })
+          console.log(req.body);
+        })
+
+        app.listen(3000, function () {
+          console.log("listening on 3000");
+        })
+  }).catch(error=> console.error(error))
